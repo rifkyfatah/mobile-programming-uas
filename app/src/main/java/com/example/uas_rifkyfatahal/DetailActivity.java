c16143e65f763363b0e5940090d6d46ae4f60799
rifkyfatah <rifkyfatahal@gmail.com>
2022-07-06 14:50:26 +0100

    Rifky UAS

---
+++ app/src/main/java/com/example/uas_rifkyfatahal/DetailActivity.java
@@ -0,0 +1,34 @@
+package com.example.uas_rifkyfatahal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    protected Cursor cursor;
+    Database database;
+    TextView nama, nim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        database = new Database(this);
+        nama = findViewById(R.id.nama);
+        nim = findViewById(R.id.nim);
+
+        SQLiteDatabase db = database.getReadableDatabase();
+        cursor = db.rawQuery("select * from tbl_mhs where nama = '"+getIntent().getStringExtra("nama")+"'", null);
+        cursor.moveToFirst();
+        if (cursor.getCount() > 0){
+            cursor.moveToPosition(0);
+            nama.setText(cursor.getString(0).toString());
+            nim.setText(cursor.getString(1).toString());
+        }
+    }
+}
